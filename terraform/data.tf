dc193b51e88f71dd62ffaddaba3e50a7493aff46
osawa-koki <github@osawa.dev>
2023-03-14 00:03:32 +0900

    小さな修正。

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,6 +1,6 @@
 
 resource "azurerm_storage_account" "storage_account" {
-  name                     = replace(var.storage_account_name, "-", "")
+  name                     = lower(replace(var.storage_account_name, "-", ""))
   resource_group_name      = azurerm_resource_group.resource_group.name
   location                 = azurerm_resource_group.resource_group.location
   account_tier             = "Standard"
