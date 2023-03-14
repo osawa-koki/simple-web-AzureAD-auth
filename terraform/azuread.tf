c371023e6146529aa94fb6f3f76bfa3e1311d8a0
osawa-koki <github@osawa.dev>
2023-03-14 21:58:17 +0900

    AzureADはなぜかダメだ、、、

--- terraform/azuread.tf
+++ terraform/azuread.tf
@@ -1,18 +1,18 @@
 
 
-resource "azuread_application" "azuread_app" {
-  display_name     = "${var.project_name}-azuread-app"
-  identifier_uris  = ["http://localhost:3000"]
-  sign_in_audience = "AzureADMultipleOrgs"
+# resource "azuread_application" "azuread_app" {
+#   display_name     = "${var.project_name}-azuread-app"
+#   identifier_uris  = ["http://localhost:3000"]
+#   sign_in_audience = "AzureADMultipleOrgs"
 
-  web {
-    homepage_url  = "${var.base_url}"
-    logout_url    = "${var.base_url}/logout"
-    redirect_uris = ["${var.base_url}/account"]
+#   web {
+#     homepage_url  = "${var.base_url}"
+#     logout_url    = "${var.base_url}/logout"
+#     redirect_uris = ["${var.base_url}/account"]
 
-    implicit_grant {
-      access_token_issuance_enabled = true
-      id_token_issuance_enabled     = true
-    }
-  }
-}
+#     implicit_grant {
+#       access_token_issuance_enabled = true
+#       id_token_issuance_enabled     = true
+#     }
+#   }
+# }
