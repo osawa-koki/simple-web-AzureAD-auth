143f76ff94a2787c773a636b24a7a4cd997854df
osawa-koki <github@osawa.dev>
2023-03-16 20:53:51 +0900

    requested_access_token_version = 2

--- terraform/azuread.tf
+++ terraform/azuread.tf
@@ -3,6 +3,10 @@ resource "azuread_application" "azuread_app" {
   display_name     = "${var.project_name}-azuread-app"
   sign_in_audience = "AzureADandPersonalMicrosoftAccount"
 
+  api {
+    requested_access_token_version = 2
+  }
+
   single_page_application {
     redirect_uris = [var.redirect_uri]
   }
