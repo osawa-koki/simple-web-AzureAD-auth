0e897160b51f9c9b5f9992dd4784e35b5be01ec8
osawa-koki <github@osawa.dev>
2023-03-16 22:11:11 +0900

    yarn prettify

--- client/MSAD.ts
+++ client/MSAD.ts
@@ -1,4 +1,4 @@
-import setting from "./setting";
+import setting from './setting';
 
 export const msalConfig = {
   auth: {
@@ -7,17 +7,17 @@ export const msalConfig = {
     redirectUri: setting.redirectUri,
   },
   cache: {
-    cacheLocation: "sessionStorage", // This configures where your cache will be stored
+    cacheLocation: 'sessionStorage', // This configures where your cache will be stored
     storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
-  }
+  },
 };
 
 // Add scopes here for ID token to be used at Microsoft identity platform endpoints.
 export const loginRequest = {
- scopes: ["User.Read"]
+  scopes: ['User.Read'],
 };
 
 // Add the endpoints here for Microsoft Graph API services you'd like to use.
 export const graphConfig = {
-  graphMeEndpoint: "Enter_the_Graph_Endpoint_Here/v1.0/me"
+  graphMeEndpoint: 'Enter_the_Graph_Endpoint_Here/v1.0/me',
 };
