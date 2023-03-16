9480e8caeaed08e61e95165f4c99068ba63e05ea
osawa-koki <github@osawa.dev>
2023-03-16 21:31:01 +0900

    説明書いた。

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -7,14 +7,8 @@ export default function AboutPage() {
       <div id="About">
         <h1>Here, About page.</h1>
         <p className="mt-3">
-          Next.js大好きです。<br />
-          本当に使いやすいです。<br />
-          僕はTypeScript × Next.jsを強くオススメします。<br />
-          <br />
-          静的型付け言語って難しいと思われがちですが、エディタの機能が豊富な現在は型付け言語の方が簡単に書くことができます。<br />
-          さらには字句解析と構文解析に加えて、コンパイル時に、コンパイラによる意味解析を行ってくれるため、動的型付け言語と比較して動作に信頼性があります。<br />
-          <br />
-          また、Reactと比べてNext.jsはSSGによってビルド時にHTMLが生成されるため、SPA(SSR)と比べて高速に動作し、ユーザビリティが高いです。
+          Azure ADを使用して、認証を行うサンプルです。<br />
+          Yeah!
         </p>
       </div>
     </Layout>
