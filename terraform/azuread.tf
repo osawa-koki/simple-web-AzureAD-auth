7b32925adf5937157fd74bad850448fae23c4fcd
osawa-koki <github@osawa.dev>
2023-03-16 20:47:36 +0900

    sign_in_audience変えた。

--- terraform/azuread.tf
+++ terraform/azuread.tf
@@ -1,7 +1,7 @@
 
 resource "azuread_application" "azuread_app" {
   display_name     = "${var.project_name}-azuread-app"
-  sign_in_audience = "AzureADMultipleOrgs"
+  sign_in_audience = "AzureADandPersonalMicrosoftAccount"
 
   single_page_application {
     redirect_uris = [var.redirect_uri]
